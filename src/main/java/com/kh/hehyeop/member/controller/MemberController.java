ee23cdde6aeba92af9d1d7a34057ad05fb90781f
ggumeng <kkh47406@naver.com>
2021-11-11 15:58:46 +0900

    [11.11 구현] member dto commit

--- src/main/java/com/kh/hehyeop/member/controller/MemberController.java
+++ src/main/java/com/kh/hehyeop/member/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.kh.hehyeop.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.hehyeop.member.model.dto.Member;
+import com.kh.hehyeop.common.validator.ValidateResult;
 
 @Controller
 @RequestMapping("member")
@@ -12,7 +16,9 @@ public class MemberController {
 	public void loginTest() {}
 	
 	@GetMapping("join-form")
-	public void JoinTest() {}
+	public void joinMember(Model model) {
+		model.addAttribute(new Member()).addAttribute("error", new ValidateResult().getError());
+	}
 	
 	@GetMapping("join-form-next")
 	public void JoinNextTest() {}
--- src/main/java/com/kh/hehyeop/member/model/dto/Member.java
+++ src/main/java/com/kh/hehyeop/member/model/dto/Member.java
@@ -1,5 +1,23 @@
 package com.kh.hehyeop.member.model.dto;
 
-public class Member {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class Member {
+	
+	private String id;
+	private String password;
+	private String name;
+	private String tell;
+	private String email;
+	private String nickname;
+	private String address;
+	private String oldAddress;
+	private int point;
+	private String grade;
+	private Date regDate;
+	private int isLeave;
+	
 }
