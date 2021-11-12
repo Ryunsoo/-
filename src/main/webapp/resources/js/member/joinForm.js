4b4cdd84bf2c430e7804d4149c48f4962bb58a41
hututi4717 <carinae4717@gmail.com>
2021-11-12 19:05:53 +0900

    [11.12] 주현 발리데이트

--- src/main/webapp/resources/js/member/joinForm.js
+++ src/main/webapp/resources/js/member/joinForm.js
@@ -201,9 +201,15 @@
 	/*전화번호 입력창 테두리*/
 	document.querySelector("#tell").addEventListener('input', e => {
 		let tellInput = document.querySelector('#tell');
-		
+		let tellExpr = /^\\d{9,11}$/;
 		if(tellInput.value == "") {
 			tellInput.style.border = '1px solid lightgray';
+			confirmTellCheck = false;
+			return;
+		}
+		
+		if(!teellExpr.test(tellInput.value)) {
+			tellInput.style.border = '1px solid red';
 			confirmTellCheck = false;
 			return;
 		}
@@ -213,6 +219,20 @@
 			confirmTellCheck = true;
 		}
 	})
+	
+	
+	
+	if(testExpr.test(passwordInput)) {
+			document.querySelector('.form-control-pw-top').style.border = '1px solid green';
+		}
+		
+		if(!testExpr.test(passwordInput)) {
+			document.querySelector('.form-control-pw-top').style.border = '1px solid red';
+		}
+		
+		if(passwordInput == "") {
+			pwInputTopReset();
+		}
 	
 	
 	/*제출버튼 검증*/
