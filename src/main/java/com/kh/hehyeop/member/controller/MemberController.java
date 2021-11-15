3f919930b987a2c3ff40836b856ffeb820fa784c
kyungmin <alssgo70051@naver.com>
2021-11-15 14:08:38 +0900

    [11.15 경민] 커밋 발사!

--- src/main/java/com/kh/hehyeop/member/controller/MemberController.java
+++ src/main/java/com/kh/hehyeop/member/controller/MemberController.java
@@ -1,15 +1,12 @@
 package com.kh.hehyeop.member.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
