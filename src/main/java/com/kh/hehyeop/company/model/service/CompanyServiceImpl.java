73a54f9fdfe54bb8b4222ea6df3f0163885ada24
drakart <oooklyt@gmail.com>
2021-11-23 18:52:38 +0900

    [11.23 현수] 헬프 끝.. 컴퍼니 시작..

---
+++ src/main/java/com/kh/hehyeop/company/model/service/CompanyServiceImpl.java
@@ -0,0 +1,11 @@
+package com.kh.hehyeop.company.model.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CompanyServiceImpl implements CompanyService{
+
+}
