1b0024f96491549bdbf2d1fff7fa1a6aab3e35e8
hututi4717 <carinae4717@gmail.com>
2021-11-24 21:34:14 +0900

    [11.24]주현 임시

--- src/main/java/com/kh/hehyeop/purchase/controller/PurchaseController.java
+++ src/main/java/com/kh/hehyeop/purchase/controller/PurchaseController.java
@@ -161,11 +161,11 @@ public class PurchaseController {
 		
 		if(nowPage == null && cntPerPage == null) {
 			nowPage = "1";
-			cntPerPage = "1";
+			cntPerPage = "5";
 		} else if (nowPage == null) {
 			nowPage = "1";
 		} else if (cntPerPage == null) { 
-			cntPerPage = "1";
+			cntPerPage = "5";
 		}
 		
 		if(ongoing != null && ongoing.equals("N")) {
