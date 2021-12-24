6b061b957fbe10d8d6cf8d89933c586cf9ebf106
lhj132824 <lhj132824@gmail.com>
2021-12-24 23:42:38 +0900

    [12.24 호준]

--- src/main/java/com/kh/hehyeop/mypage/controller/MypageController.java
+++ src/main/java/com/kh/hehyeop/mypage/controller/MypageController.java
@@ -553,7 +553,7 @@ public class MypageController {
 			session.setAttribute("authentication", authentication);
 			session.setAttribute("myField", myField);
 			     
-			  
+			   
 			redirectAttr.addFlashAttribute("message", "수정이 완료 되었습니다.");
 			return "redirect:/mypage/mypage-company";
 		}
